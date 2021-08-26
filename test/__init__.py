54630ab5f8f03890696d2f80276f8dcb346a214e
Elias Koromilas <elias.koromilas@gmail.com>
2021-08-26 16:40:16 +0300

    Add null allocator example

--- test/__init__.py
+++ test/__init__.py
@@ -1,2 +1,3 @@
 from .aligned_allocator import aligned_allocator
 from .debug_allocator import debug_allocator
+from .null_allocator import null_allocator
---
+++ test/null_allocator.py
@@ -0,0 +1,45 @@
+from ctypes import *
+from numpy_allocator import base_allocator
+
+
+@CFUNCTYPE(c_void_p, c_size_t, c_size_t)
+def null_calloc(nelem, elsize):
+    return None
+
+
+@CFUNCTYPE(None, c_void_p, c_size_t)
+def null_free(ptr, size):
+    pass
+
+
+@CFUNCTYPE(c_void_p, c_size_t)
+def null_malloc(size):
+    return None
+
+
+@CFUNCTYPE(c_void_p, c_void_p, c_size_t)
+def null_realloc(ptr, new_size):
+    return None
+
+
+class null_allocator(metaclass=base_allocator):
+
+    _calloc_ = null_calloc
+
+    _free_ = null_free
+
+    _malloc_ = null_malloc
+
+    _realloc_ = null_realloc
+
+
+def main():
+    import numpy as np
+
+    with np.testing.assert_raises(MemoryError):
+        with null_allocator:
+            np.ndarray(())
+
+
+if __name__ == '__main__':
+    main()
